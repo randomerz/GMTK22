e0beabd2468b83ec898b8d21127e1a256ec0f1ec
travis-bittel <tbittel6@gatech.edu>
2022-07-15 23:19:59 -0400

    Add Subtitles

--- OutofPocket/Assets/UIManager.cs
+++ OutofPocket/Assets/UIManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private GameObject mainPanel;
+    [SerializeField] private TextMeshProUGUI subtitleText;
+
+    private void Awake()
+    {
+        InitializeSingleton();
+    }
 
     private void Update()
     {
@@ -12,5 +19,21 @@ public class UIManager : Singleton<UIManager>
         {
             mainPanel.SetActive(!mainPanel.activeInHierarchy);
         }
+    }
+
+    public static void SetSubtitle(string text, Color color, float duration)
+    {
+        _instance.StartCoroutine(_instance.ISetSubtitleForDuration(text, color, duration));
+    }
+
+    private IEnumerator ISetSubtitleForDuration(string text, Color color, float duration)
+    {
+        subtitleText.text = text;
+        subtitleText.color = color;
+
+        yield return new WaitForSeconds(duration);
+
+        subtitleText.text = "";
+        subtitleText.color = Color.white;
     }
 }
