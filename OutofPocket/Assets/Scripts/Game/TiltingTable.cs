22922445ba361e8552b6e141cdfcf9d0c62e991a
AneeshBoreda <aneesh.boreda92@gmail.com>
2022-07-16 02:21:18 -0400

    fixed tilt stuff

--- OutofPocket/Assets/Scripts/Game/TiltingTable.cs
+++ OutofPocket/Assets/Scripts/Game/TiltingTable.cs
@@ -4,38 +4,62 @@ using UnityEngine;
 
 public class TiltingTable : MonoBehaviour
 {
-    public float maxTilt;
 
-    private float accel = 0.5f;
+    public float accel = 5f;
+    public float maxXTilt = 10f; //degrees
+    public float maxZTilt = 5f; // degrees
+
+    private float lastXTarget = 0;
+    private float lastZTarget = 0;
+    //private float xt=0;
+    //private float zt=0;
+    private float t = 0;
+    private Quaternion startRot;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startRot = transform.rotation;
     }
 
     // Update is called once per frame
     // Doesnt work
     void Update()
     {
-        float xtilt = 10f;
-        float ytilt = 0f;
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        //float xtilt = 10f;
+        //float ztilt = 0f;
+        float xTarget = 0;
+        float zTarget = 0;
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("Hello!");
-            xtilt += -maxTilt;
+            //Debug.Log("Hello!");
+            xTarget += -maxXTilt;
+            //Debug.Log(xtilt);
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            xtilt += maxTilt;
+            xTarget += maxXTilt;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            ytilt += maxTilt;
+            zTarget += -maxZTilt;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            ytilt += -maxTilt;
+            zTarget += maxZTilt;
         }
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(xtilt, ytilt, 0f), Time.deltaTime * accel);
+        //Debug.Log(xtilt);
+        if(lastXTarget != xTarget || lastZTarget != zTarget) {
+            startRot = transform.rotation;
+            t=0;
+            //Debug.Log("Resetting Target");
+        }
+
+        transform.rotation = Quaternion.Slerp(startRot, Quaternion.Euler(xTarget, 0, zTarget), t);
+        //Debug.Log(Quaternion.Slerp(startRot, Quaternion.Euler(xTarget, 0, zTarget), t));
+        //xt += Time.deltaTime * accel;
+        //zt += Time.deltaTime * accel;
+        t += Time.deltaTime * accel;
+        lastXTarget = xTarget;
+        lastZTarget = zTarget;
     }
 }
