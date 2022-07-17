fbef1a54ba0dfde65b599641111216c97d60d6ac
AneeshBoreda <aneesh.boreda92@gmail.com>
2022-07-17 10:15:05 -0400

    joooooooooooooooooooooooooooooooooooooooooooooooooooooooooooos

--- OutofPocket/Assets/Scenes/kenneth/Juicer.cs
+++ OutofPocket/Assets/Scenes/kenneth/Juicer.cs
@@ -38,39 +38,45 @@ public class Juicer : MonoBehaviour
     }
 
     private void OnHit(object sender, CueBall.BallShotEventArgs e) {
-        CameraShake.Shake(juiceMultiplier,0.8f);
-        Smack();
+        if (juiceMultiplier > 0.2f){
+            CameraShake.Shake(juiceMultiplier,0.8f);
+            Smack();
+        }
     }
 
     private void OnPocket(object sender, PoolBall.BallEventArgs e) {
-        CameraShake.Shake(juiceMultiplier,1.5f);
-        Yay(e.pocket);
+        if (juiceMultiplier > 0.2f){
+            CameraShake.Shake(juiceMultiplier,1.5f);
+            Yay(e.pocket);
+        }
     }
 
     private void OnCollisionEnter(Collision other) {
-        if (other.gameObject.CompareTag("PoolBall")) {
-            float mag = other.relativeVelocity.magnitude;
-            if(mag > 20) {
-                CameraShake.Shake(juiceMultiplier,0.7f);
-                Spark(1.8f, 60); 
-            } else if(mag>10) {
-                CameraShake.Shake(juiceMultiplier,0.5f);
-                Spark(1.0f, 30); 
-            } else {
-                CameraShake.Shake(juiceMultiplier,0.2f);
-                Spark(0.2f, 5); 
-            }  
+        if (juiceMultiplier > 0.2f){
+            if (other.gameObject.CompareTag("PoolBall")) {
+                float mag = other.relativeVelocity.magnitude;
+                if(mag > 20) {
+                    CameraShake.Shake(juiceMultiplier,0.7f);
+                    Spark(1.8f, 60); 
+                } else if(mag>10) {
+                    CameraShake.Shake(juiceMultiplier,0.5f);
+                    Spark(1.0f, 30); 
+                } else {
+                    CameraShake.Shake(juiceMultiplier,0.2f);
+                    Spark(0.2f, 5); 
+                }  
+            }
         }
     }
 
     private void Smack() {
-        if(juiceMultiplier > 0.0f) {
+        if(juiceMultiplier > 0.4f) {
             cueHitParticles.Play();
         }
     }
     
     private void Yay(GameObject pkt) {
-        if(juiceMultiplier > 0.0f) {
+        if(juiceMultiplier > 0.4f) {
             ParticleSystem sparkles = pkt.GetComponentInChildren<ParticleSystem>();;
             var sparklesMain = sparkles.main;
             var sparkleEm = sparkles.emission;
@@ -84,7 +90,7 @@ public class Juicer : MonoBehaviour
         }
     }
     private void Spark(float modifier,short numParticles) {
-        if(juiceMultiplier > 0.0f) {
+        if(juiceMultiplier > 0.4f) {
             var em = impactParticleSystem.emission;
             var main = impactParticleSystem.main;
             main.startSpeed = Random.Range(1.0f, 20.0f * modifier * juiceMultiplier);
--- OutofPocket/Assets/Scripts/Game/GameManager.cs
+++ OutofPocket/Assets/Scripts/Game/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager : Singleton<GameManager>
     public GameObject poolBallTriangle;
     public GameObject cueBall;
     public GameObject inGameUI;
+    public Juicer joooooooooooooooooooooos;
 
 
 
@@ -88,6 +89,7 @@ public class GameManager : Singleton<GameManager>
 
         public IEnumerator DoAct1()
         {
+            Juicer jooooooooooooooooooooos = context.joooooooooooooooooooooos;
             context.floor.SetActive(false);
             context.poolTable.SetActive(false);
             context.poolBallTriangle.SetActive(false);
@@ -299,13 +301,14 @@ public class GameManager : Singleton<GameManager>
 
             //Screenshake is added (ADD THIS!!!!!!!!!!!!!!)
             AudioManager.SetMusicParameter("High Rollers", "Juice", 1);
-
+            jooooooooooooooooooooos.juiceMultiplier=0.25f;
             //Oppy: and now, the cherry on top: , and just a little bitta particle effects as the cherry on top!
             context.DoNarrationAndSetFlag("Act1/Oppy/037_ParticleEffects");
             yield return new WaitUntil(() => { return context.currNarrationFinished; });
 
             //Particle effects added (ADD THIS!!!!!!!!!!!!!!)
             AudioManager.SetMusicParameter("High Rollers", "Juice", 2);
+            jooooooooooooooooooooos.juiceMultiplier=0.5f;
 
             //Oppy: mwa! Magnifique!
             context.DoNarrationAndSetFlag("Act1/Oppy/038_Magnifique");
@@ -360,6 +363,8 @@ public class GameManager : Singleton<GameManager>
         {
             //Disable tilting
             PoolStateManager._instance.TiltingEnabled = false;
+            context.joooooooooooooooooooooos.juiceMultiplier=0f;
+
 
             // Oppy: Well, looks like it�s just you and me for a little bit. I guess now�s a good time to get some feedback on the game! It�s not over or anything, I just thought it�d be nice right now.
             context.DoNarrationAndSetFlag("Act2/Oppy/001_AloneTime");
@@ -753,6 +758,7 @@ public class GameManager : Singleton<GameManager>
             context.DoNarrationAndSetFlag("Act3/Cynic/18_MoreMechanics");
             yield return new WaitUntil(() => { return context.currNarrationFinished; });
             //All the pockets get marked with “I’m still here”
+            yield return new WaitForSeconds(2);
             SetUpChoice("I'm still here", "I'm still here");
             
             //*silence*
@@ -793,6 +799,7 @@ public class GameManager : Singleton<GameManager>
 
 
             //The game becomes regular pool except with different physics on all the balls.
+            PoolStateManager.ResetGame();
             EndOfGame._instance.EndOfGameify();
 
             //Oppy: It’s something I came up with when we were talking. All the balls have slightly different interactions with everything.
